54757e267406e6716247725da3e0e356c8eb7a11
manojbillionaire <manojbillionaire123@gmail.com>
2026-03-09 07:05:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:3001' } },
+  server: { proxy: { '/api': 'http://localhost:8080' } },
   build: { outDir: 'dist' }
 });
